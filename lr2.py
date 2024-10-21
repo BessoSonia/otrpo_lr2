a0c646d9202c53838650f4d7937a2ffd75f844e7
BessoSonia <besso.sonia@mail.ru>
2024-10-21 17:24:17 +0500

    Скорректирован вывод

--- lr2.py
+++ lr2.py
@@ -13,13 +13,4 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # Детектирование лиц
 faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
-# Рисуем прямоугольники вокруг лиц
-for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-# Отображение изображения с детектированными лицами
-cv2.imshow('Image with faces', image)
-
-# Ожидание нажатия любой клавиши и закрытие окна
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+print(f"Обнаружено лиц: {len(faces)}")
